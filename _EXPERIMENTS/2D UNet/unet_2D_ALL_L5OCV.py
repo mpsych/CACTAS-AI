e6c72cafa0f2bb68bedd2d4be156e40bc5d52fc4
Jenna <54910137+jiehyunjkim@users.noreply.github.com>
2024-10-05 20:27:43 -0400

    LOOCV code check

--- _EXPERIMENTS/2D UNet/unet_2D_ALL_L5OCV.py
+++ _EXPERIMENTS/2D UNet/unet_2D_ALL_L5OCV.py
@@ -4,6 +4,10 @@ from keras import backend as K
 from sklearn.model_selection import KFold, train_test_split
 import numpy as np
 import sys
+import tensorflow as tf
+import pickle
+from tensorflow.keras.models import load_model
+
 
 DATAPATH='/raid/mpsych/CACTAS/DATA/'
 CAPATHS = {
@@ -39,36 +43,72 @@ X_train_array, y_train_array, X_val_array, y_val_array, X_test_array, y_test_arr
 K.clear_session()
 
 model = C.Helper.create_unet(X_train_array[0].shape)
-history = model.fit(X_train_array, y_train_array, 
-                    epochs=200, 
-                    batch_size=16, 
-                    validation_data=(X_val_array, y_val_array))
+model, history = C.Helper.train_unet(X_train_array, y_train_array, X_val_array, y_val_array, model, epochs=200)
 
 
+history_dict = history.history
+
+with open('/raid/mpsych/CACTAS/DATA/OUTPUT/training_history_V4.pkl', 'wb') as f:
+    pickle.dump(history_dict, f)
+
+model.save('/raid/mpsych/CACTAS/DATA/OUTPUT/model_LOOCV_V4.h5')
+
+
+
+
+
+
+
+
+### delete training data and model to free memory
+del X_train_array, y_train_array, X_val_array, y_val_array
+tf.keras.backend.clear_session()
+del model
+
+
+### load saved model
+from keras_unet.metrics import iou, iou_thresholded
+from tensorflow.keras.optimizers import Adam
+
+model = load_model('/raid/mpsych/CACTAS/DATA/OUTPUT/model_LOOCV_V4.h5'
+                   , custom_objects={'iou': iou, 'iou_thresholded': iou_thresholded})
+model.compile(optimizer=Adam(learning_rate=0.001),loss='binary_crossentropy',metrics=[iou, iou_thresholded])
+
+
+
+#### preiction
 y_pred = model.predict(X_test_array)
-#print("pass1")
-loss, iou, iou_thresholded = model.evaluate(X_test_array, y_test_array, verbose=0)
-#print("pass2")
+loss, iou_val, iou_thresholded_val = model.evaluate(X_test_array, y_test_array, verbose=1)
 
 print("loss: " + str(loss))
-print("iou: " + str(iou)) 
-print("iou_thresholded: " + str(iou_thresholded))
+print("iou: " + str(iou_val)) 
+print("iou_thresholded: " + str(iou_thresholded_val))
 
 
+### thresholded prediction
 a = y_pred
 a_binary = np.zeros(a.shape, dtype=np.bool_)
 a_binary[a > 0.5] = True
     
-loss_t, iou_t, iou_thresholded_t = model.evaluate(X_test_array, a_binary, verbose=0)
-#print("pass3")
+loss_t, iou_t_val, iou_thresholded_t_val = model.evaluate(X_test_array, a_binary, verbose=1)
 
 print("Thresholded loss: " + str(loss_t))
-print("Thresholded IoU: " + str(iou_t)) 
-print("Thresholded IoU_thresholded: " + str(iou_thresholded_t))
+print("Thresholded IoU: " + str(iou_t_val)) 
+print("Thresholded IoU_thresholded: " + str(iou_thresholded_t_val))
 
 
+#### save as pickl
+metrics = {
+    "loss": loss,
+    "iou": iou_val,
+    "iou_thresholded": iou_thresholded_val,
+    "Thresholded loss": loss_t,
+    "Thresholded IoU": iou_t_val,
+    "Thresholded IoU_thresholded": iou_thresholded_t_val
+}
 
-
+with open('/raid/mpsych/CACTAS/DATA/OUTPUT/metrics_V4.pkl', 'wb') as f:
+    pickle.dump(metrics, f)
 
 
 
